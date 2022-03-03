ed9ed983378762fb1e7aea5c18f5740933f4df6a
AntonWarnstrom <anton.warnstrom@ha.ax>
2022-03-03 13:19:00 +0200

    Refactor to support multiple numbers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 export class StringCalculator {
-  static Add(numbersString: string) {
+  static async Add(numbersString: string) {
     if (numbersString.length === 0) return 0;
     if (numbersString.length === 1) return parseInt(numbersString);
-    const [first, second]: number[] = numbersString.split(",").map(Number);
-    return first + second;
+    const sum: number = numbersString
+      .split(",")
+      .map(Number)
+      .reduce((a, b) => a + b);
+    return sum;
   }
 }
 StringCalculator.Add("1,2");
--- src/tests/calc.test.ts
+++ src/tests/calc.test.ts
@@ -1,12 +1,26 @@
 import { StringCalculator } from "..";
 
-test("Test for Add method", () => {
-  const test_1 = StringCalculator.Add("1");
-  expect(test_1).toBe(1);
+describe("Test for Add method", () => {
+  it("Should return 1 for add(1)", async () => {
+    const test_1 = await StringCalculator.Add("1");
+    expect(test_1).toBe(1);
+  });
 
-  const test_2 = StringCalculator.Add("");
-  expect(test_2).toBe(0);
+  it("Should return 100 for add(100)", async () => {
+    const test_1 = await StringCalculator.Add("100");
+    expect(test_1).toBe(100);
+  });
 
-  const test_3 = StringCalculator.Add("1,2");
-  expect(test_3).toBe(3);
+  it("Should return 0 for add('')", async () => {
+    const test_2 = await StringCalculator.Add("");
+    expect(test_2).toBe(0);
+  });
+  it("Should return 3 for add(1,2)", async () => {
+    const test_3 = await StringCalculator.Add("1,2");
+    expect(test_3).toBe(3);
+  });
+  it("Should return 10 for add(1,2,3,4)", async () => {
+    const test_4 = await StringCalculator.Add("1,2,3,4");
+    expect(test_4).toBe(10);
+  });
 });
