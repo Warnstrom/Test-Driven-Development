90863c45f0278f2cd7babd3a1a38ef54034da921
AntonWarnstrom <anton.warnstrom@ha.ax>
2022-03-03 15:47:40 +0200

    Refactor to support newline separator with Regex

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 export class StringCalculator {
-  static async Add(numbersString: string) {
+  static Add(numbersString: string): number {
     if (numbersString.length === 0) return 0;
     if (numbersString.length === 1) return parseInt(numbersString);
     const sum: number = numbersString
-      .split(",")
+      .split(/[\n,]/)
       .map(Number)
       .reduce((a, b) => a + b);
     return sum;
   }
 }
-StringCalculator.Add("1,2");
