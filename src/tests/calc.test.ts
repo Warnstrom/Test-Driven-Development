faf5424b6f429cc2af8d1c0d120343a1b93cce22
AntonWarnstrom <anton.warnstrom@ha.ax>
2022-03-03 15:29:54 +0200

    Added tests for newline separator in string

--- src/tests/calc.test.ts
+++ src/tests/calc.test.ts
@@ -1,26 +1,35 @@
 import { StringCalculator } from "..";
 
 describe("Test for Add method", () => {
-  it("Should return 1 for add(1)", async () => {
-    const test_1 = await StringCalculator.Add("1");
-    expect(test_1).toBe(1);
+  it("Should return 1 for add(1)", () => {
+    const sum = StringCalculator.Add("1");
+    expect(sum).toBe(1);
   });
 
-  it("Should return 100 for add(100)", async () => {
-    const test_1 = await StringCalculator.Add("100");
-    expect(test_1).toBe(100);
+  it("Should return 100 for add(100)", () => {
+    const sum = StringCalculator.Add("100");
+    expect(sum).toBe(100);
   });
 
-  it("Should return 0 for add('')", async () => {
-    const test_2 = await StringCalculator.Add("");
-    expect(test_2).toBe(0);
+  it("Should return 0 for add('')", () => {
+    const sum = StringCalculator.Add("");
+    expect(sum).toBe(0);
   });
-  it("Should return 3 for add(1,2)", async () => {
-    const test_3 = await StringCalculator.Add("1,2");
-    expect(test_3).toBe(3);
+  it("Should return 3 for add(1,2)", () => {
+    const sum = StringCalculator.Add("1,2");
+    expect(sum).toBe(3);
   });
-  it("Should return 10 for add(1,2,3,4)", async () => {
-    const test_4 = await StringCalculator.Add("1,2,3,4");
-    expect(test_4).toBe(10);
+  it("Should return 10 for add(1,2,3,4)", () => {
+    const sum = StringCalculator.Add("1,2,3,4");
+    expect(sum).toBe(10);
+  });
+
+  it("Should return correct sum with newline separator", () => {
+    const sum = StringCalculator.Add("1\n2,4");
+    expect(sum).toBe(7);
+  });
+  it("Should return correct sum with newline separator", () => {
+    const sum = StringCalculator.Add("1\n2\n4");
+    expect(sum).toBe(7);
   });
 });
