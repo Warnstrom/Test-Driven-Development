bab279c77653bfe42c750bb5e1c924f58864950e
AntonWarnstrom <anton.warnstrom@ha.ax>
2022-03-14 01:08:08 +0200

    Implemented code for tests

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,92 @@
-export class MarsRover {}
-export class Planet {}
+export class Coordinates {
+  constructor(readonly x: number, readonly y: number) {}
+}
+export class MarsRover {
+  private planet: [number, number] = [5, 5];
+  private currentOrientation: string;
+  private possibleOrientations: string[] = ["N", "E", "S", "W"];
+  private moveInstructions: { [command: string]: () => Boolean } = {
+    F: () => this.forward(),
+    B: () => this.back(),
+    R: () => this.right(),
+    L: () => this.left(),
+  };
+  private position: { [key: string]: number } = {
+    x: 0,
+    y: 0,
+  };
+
+  constructor(x: number, y: number, orientation: string) {
+    this.position["x"] = x;
+    this.position["y"] = y;
+    this.currentOrientation = orientation;
+  }
+
+  getOrientation(): string {
+    return this.currentOrientation;
+  }
+  getPosition() {
+    return this.position;
+  }
+  move(instructions: string): void {
+    for (const command of [...instructions]) {
+      if (!this.moveInstructions[command]()) {
+        throw new Error("Something went wrong");
+      }
+    }
+  }
+  private forward(): boolean {
+    this.position["y"] =
+      this.currentOrientation === "N" && this.position["y"] === 4 ? 0 : (this.position["y"] += 1);
+    this.position["y"] =
+      this.currentOrientation === "S" && this.position["y"] === 0 ? 4 : this.position["y"]--;
+    this.position["x"] =
+      this.currentOrientation === "W" && this.position["x"] === 0 ? 4 : this.position["x"]--;
+    this.position["x"] =
+      this.currentOrientation === "E" && this.position["x"] === 4 ? 0 : this.position["x"]++;
+    return true;
+  }
+  private back(): boolean {
+    this.position["y"] =
+      this.currentOrientation === "N" && this.position["y"] === 0 ? 4 : this.position["y"]--;
+    this.position["y"] =
+      this.currentOrientation === "S" && this.position["y"] === 4 ? 0 : this.position["y"]++;
+    this.position["x"] =
+      this.currentOrientation === "E" && this.position["x"] === 0 ? 4 : this.position["x"]--;
+    this.position["x"] =
+      this.currentOrientation === "W" && this.position["x"] === 4 ? 0 : this.position["x"]++;
+    return true;
+  }
+  /**
+   *
+   * Ugly if/if else statement, not proud at all
+   */
+  private right(): boolean {
+    if (this.currentOrientation === "N") {
+      this.currentOrientation = "E";
+    } else if (this.currentOrientation === "E") {
+      this.currentOrientation = "S";
+    } else if (this.currentOrientation === "S") {
+      this.currentOrientation = "W";
+    } else if (this.currentOrientation === "W") {
+      this.currentOrientation = "N";
+    }
+    return true;
+  }
+  /**
+   *
+   * Ugly if/if else statement, not proud at all
+   */
+  private left(): boolean {
+    if (this.currentOrientation === "N") {
+      this.currentOrientation = "W";
+    } else if (this.currentOrientation === "W") {
+      this.currentOrientation = "S";
+    } else if (this.currentOrientation === "S") {
+      this.currentOrientation = "E";
+    } else if (this.currentOrientation === "E") {
+      this.currentOrientation = "N";
+    }
+    return true;
+  }
+}
--- src/tests/rover.test.ts
+++ src/tests/rover.test.ts
@@ -1,46 +1,50 @@
 import { MarsRover } from "../index";
-import { Planet } from "../index";
 
-describe("Mars Rover Tests", () => {
-  const planet: Planet = new Planet(5, 5);
-  const rover: MarsRover = new MarsRover(1, 2);
-  describe("Create mars rover", () => {
-    it("should return starting position with the direction of the rover", () => {
-      const position: RoverPosition = rover.getPosition();
-      const direction: RoverDirection = rover.getDirection();
-      expect(position.x).toBe(1);
-      expect(position.y).toBe(2);
-      expect(direction).toBe("N");
-    });
+describe("Create mars rover", () => {
+  it("should return starting position with the direction of the rover", () => {
+    const rover: MarsRover = new MarsRover(0, 0, "N");
+    expect(rover).toBeDefined();
+    rover.move("FFLFRF");
+    const position = rover.getPosition();
+    const direction: string = rover.getOrientation();
+    expect(position.x).toBe(4);
+    expect(position.y).toBe(3);
+    expect(direction).toBe("N");
   });
-  describe("Move the rover forward", () => {
-    it("should return x+1 with the direction of the rover", () => {
-      rover.execute("move");
-      const position: RoverPosition = rover.getPosition();
-      const direction: RoverDirection = rover.getDirection();
-      expect(position.x).toBe(2);
-      expect(position.y).toBe(2);
-      expect(direction).toBe("N");
-    });
+});
+describe("Move the rover forward", () => {
+  it("should return x+1 with the direction of the rover", () => {
+    const rover: MarsRover = new MarsRover(0, 0, "N");
+    expect(rover).toBeDefined();
+    rover.move("RFFLFF");
+    const position = rover.getPosition();
+    const direction: string = rover.getOrientation();
+    expect(position.x).toBe(2);
+    expect(position.y).toBe(2);
+    expect(direction).toBe("N");
   });
-  describe("Turn the rover to the right", () => {
-    it("should return x+1 with the direction of the rover", () => {
-      rover.execute("turnRight");
-      const position: RoverPosition = rover.getPosition();
-      const direction: RoverDirection = rover.getDirection();
-      expect(position.x).toBe(2);
-      expect(position.y).toBe(2);
-      expect(direction).toBe("E");
-    });
+});
+describe("Turn the rover to the right", () => {
+  it("should return x+1 with the direction of the rover", () => {
+    const rover: MarsRover = new MarsRover(0, 0, "N");
+    expect(rover).toBeDefined();
+    rover.move("LF");
+    const position = rover.getPosition();
+    const direction: string = rover.getOrientation();
+    expect(position.x).toBe(4);
+    expect(position.y).toBe(0);
+    expect(direction).toBe("W");
   });
-  describe("Reverse rover one", () => {
-    it("should return x-1 with the direction of the rover", () => {
-      rover.execute("reverse");
-      const position: RoverPosition = rover.getPosition();
-      const direction: RoverDirection = rover.getDirection();
-      expect(position.x).toBe(1);
-      expect(position.y).toBe(2);
-      expect(direction).toBe("E");
-    });
+});
+describe("Reverse rover one", () => {
+  it("should return x-1 with the direction of the rover", () => {
+    const rover: MarsRover = new MarsRover(0, 0, "N");
+    expect(rover).toBeDefined();
+    rover.move("RFFFLFRFF");
+    const position = rover.getPosition();
+    const direction: string = rover.getOrientation();
+    expect(position.x).toBe(0);
+    expect(position.y).toBe(1);
+    expect(direction).toBe("E");
   });
 });
